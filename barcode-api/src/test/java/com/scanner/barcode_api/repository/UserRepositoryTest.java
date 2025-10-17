bf005f2a0ce8150ba2f4c49e2a6cce1fdaa590d6
iruy <yuri_matteus@hotmail.com>
2025-10-17 16:55:34 -0300

    setting prod on env

---
+++ barcode-api/src/test/java/com/scanner/barcode_api/repository/UserRepositoryTest.java
@@ -0,0 +1,15 @@
+package com.scanner.barcode_api.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+
+    @Test
+    void findByEmail() {
+
+        
+
+    }
+}
